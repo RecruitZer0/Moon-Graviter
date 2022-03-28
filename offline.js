15307a255d3d0ee5a9577c6d88d77f3334b2d404
Recruit_Zer0 <59139794+RecruitZer0@users.noreply.github.com>
2022-03-27 22:26:20 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1648430683,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/terra-sheet0.png",
+		"images/bola-sheet0.png",
+		"images/buraco-sheet0.png",
+		"images/trajetoria-sheet0.png",
+		"images/fundosemitransparente.png",
+		"images/terrasemitransparente-sheet0.png",
+		"images/luasemitransparente-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
